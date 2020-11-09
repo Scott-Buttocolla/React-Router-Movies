cd0cc632ba058516d9b8853cb4dc465050e0f90e
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-11-09 11:35:30 -0800

    added to movielist file the Link tag, links now work to add to thge card

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -15,6 +15,7 @@ function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
   return (
+    <Link to={`/movies/${props.movie.id}`}>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -24,5 +25,6 @@ function MovieDetails(props) {
         Metascore: <strong>{metascore}</strong>
       </div>
     </div>
+    </Link>
   );
 }
